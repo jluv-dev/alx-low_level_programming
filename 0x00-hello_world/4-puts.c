7bad7c7095a7eefca64c7684e581bc08011435e8
Juliet Igiebor <julietigiebor@gmail.com>
2022-06-06 13:03:54 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  *main - Entry point
  *
- *Return: Always O (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
  puts("\"Programming is like building a multilingual puzzle");
- return (O);
+ return (0);
 }
